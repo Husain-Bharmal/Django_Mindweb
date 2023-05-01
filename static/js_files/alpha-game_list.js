79b0ad05873a6e5be91063a69a372df034b5aff8
ANKIT1610 <aab2@somaiya.edu>
2023-05-01 10:52:39 +0530

    alpha-game ko barobar kr diya images aa rhe h ab

--- static/js_files/alpha-game_list.js
+++ static/js_files/alpha-game_list.js
@@ -1,79 +1,79 @@
-const imglist = [['./assets/alphabets/A-img/apple.jpg', 'apple'],
-['./assets/alphabets/A-img/Aeroplane.jpg', 'aeroplane'],
-['./assets/alphabets/A-img/Arrow.jpg', 'arrow'],
+const imglist = [['/static/assets/alphabets/A-img/apple.jpg', 'apple'],
+['/static/assets/alphabets/A-img/Aeroplane.jpg', 'aeroplane'],
+['/static/assets/alphabets/A-img/Arrow.jpg', 'arrow'],
 
-['./assets/alphabets/B-img/ball', 'ball'],
-['./assets/alphabets/B-img/bird', 'bird'],
+['/static/assets/alphabets/B-img/ball', 'ball'],
+['/static/assets/alphabets/B-img/bird', 'bird'],
 
-['./assets/alphabets/C-img/cat.png', 'cat'],
-['./assets/alphabets/C-img/cow.jpg', 'cow'],
+['/static/assets/alphabets/C-img/cat.png', 'cat'],
+['/static/assets/alphabets/C-img/cow.jpg', 'cow'],
 
-['./assets/alphabets/D-img/dog.jpg', 'dog'],
-['./assets/alphabets/D-img/doll', 'doll'],
+['/static/assets/alphabets/D-img/dog.jpg', 'dog'],
+['/static/assets/alphabets/D-img/doll', 'doll'],
 
-['./assets/alphabets/E-img/elephant.jpg', 'elephant'],
-['./assets/alphabets/E-img/egg.png', 'egg'],
+['/static/assets/alphabets/E-img/elephant.jpg', 'elephant'],
+['/static/assets/alphabets/E-img/egg.png', 'egg'],
 
-['./assets/alphabets/F-img/fish.png', 'fish'],
-['./assets/alphabets/F-img/fan.jpg', 'fan'],
+['/static/assets/alphabets/F-img/fish.png', 'fish'],
+['/static/assets/alphabets/F-img/fan.jpg', 'fan'],
 
-['./assets/alphabets/G-img/grapes.jpg', 'grapes'],
-['./assets/alphabets/G-img/goat.jpg', 'goat'],
+['/static/assets/alphabets/G-img/grapes.jpg', 'grapes'],
+['/static/assets/alphabets/G-img/goat.jpg', 'goat'],
 
-['./assets/alphabets/H-img/horse.jpg', 'horse'],
-['./assets/alphabets/H-img/hen.jpg', 'hen'],
+['/static/assets/alphabets/H-img/horse.jpg', 'horse'],
+['/static/assets/alphabets/H-img/hen.jpg', 'hen'],
 
-['./assets/alphabets/I-img/ice-cream.png', 'icecream'],
-['./assets/alphabets/I-img/ink.jpg', 'ink'],
+['/static/assets/alphabets/I-img/ice-cream.png', 'icecream'],
+['/static/assets/alphabets/I-img/ink.jpg', 'ink'],
 
-['./assets/alphabets/J-img/joker.png', 'joker'],
-['./assets/alphabets/J-img/jug.png', 'jug'],
+['/static/assets/alphabets/J-img/joker.png', 'joker'],
+['/static/assets/alphabets/J-img/jug.png', 'jug'],
 
-['./assets/alphabets/K-img/kite.png', 'kite'],
-['./assets/alphabets/K-img/kangaroo.png', 'kangaroo'],
+['/static/assets/alphabets/K-img/kite.png', 'kite'],
+['/static/assets/alphabets/K-img/kangaroo.png', 'kangaroo'],
 
-['./assets/alphabets/L-img/lion.png', 'lion'],
-['./assets/alphabets/L-img/lamp.png', 'lamp'],
+['/static/assets/alphabets/L-img/lion.png', 'lion'],
+['/static/assets/alphabets/L-img/lamp.png', 'lamp'],
 
-['./assets/alphabets/M-img/mango.jpg', 'mango'],
-['./assets/alphabets/M-img/monkey.png', 'monkey'],
+['/static/assets/alphabets/M-img/mango.jpg', 'mango'],
+['/static/assets/alphabets/M-img/monkey.png', 'monkey'],
 
-['./assets/alphabets/N-img/net.png', 'net'],
-['./assets/alphabets/N-img/neck.jpg', 'neck'],
+['/static/assets/alphabets/N-img/net.png', 'net'],
+['/static/assets/alphabets/N-img/neck.jpg', 'neck'],
 
-['./assets/alphabets/O-img/orange.png', 'orange'],
-['./assets/alphabets/O-img/owl.jpg', 'owl'],
+['/static/assets/alphabets/O-img/orange.png', 'orange'],
+['/static/assets/alphabets/O-img/owl.jpg', 'owl'],
 
-['./assets/alphabets/P-img/parrot.jpg', 'parrot'],
-['./assets/alphabets/P-img/pen.jpg', 'pen'],
+['/static/assets/alphabets/P-img/parrot.jpg', 'parrot'],
+['/static/assets/alphabets/P-img/pen.jpg', 'pen'],
 
-['./assets/alphabets/Q-img/queen.jpg', 'queen'],
+['/static/assets/alphabets/Q-img/queen.jpg', 'queen'],
 // ['Images/E-img/elephant.jpg','elephant' ],
 
-['./assets/alphabets/R-img/rabbit.jpg', 'rabbit'],
-['./assets/alphabets/R-img/robot.png', 'robot'],
+['/static/assets/alphabets/R-img/rabbit.jpg', 'rabbit'],
+['/static/assets/alphabets/R-img/robot.png', 'robot'],
 
-['./assets/alphabets/S-img/sun.png', 'sun'],
-['./assets/alphabets/S-img/steel.jpg', 'steel'],
+['/static/assets/alphabets/S-img/sun.png', 'sun'],
+['/static/assets/alphabets/S-img/steel.jpg', 'steel'],
 
-['./assets/alphabets/T-img/table.jpg', 'table'],
-['./assets/alphabets/T-img/top.jpg', 'top'],
+['/static/assets/alphabets/T-img/table.jpg', 'table'],
+['/static/assets/alphabets/T-img/top.jpg', 'top'],
 
-['./assets/alphabets/U-img/umbrella.png', 'umbrella'],
-['./assets/alphabets/U-img/unicorn.webp', 'unicorn'],
+['/static/assets/alphabets/U-img/umbrella.png', 'umbrella'],
+['/static/assets/alphabets/U-img/unicorn.webp', 'unicorn'],
 
-['./assets/alphabets/V-img/van.png', 'van'],
-['./assets/alphabets/V-img/violin.png', 'violin'],
+['/static/assets/alphabets/V-img/van.png', 'van'],
+['/static/assets/alphabets/V-img/violin.png', 'violin'],
 
-['./assets/alphabets/W-img/watch4.png', 'watch'],
-['./assets/alphabets/W-img/wire.jpg', 'wire'],
+['/static/assets/alphabets/W-img/watch4.png', 'watch'],
+['/static/assets/alphabets/W-img/wire.jpg', 'wire'],
 
-['./assets/alphabets/X-img/xmas-tree.png', 'xmastree'],
-['./assets/alphabets/X-img/xylophone.jpg', 'xylophone'],
+['/static/assets/alphabets/X-img/xmas-tree.png', 'xmastree'],
+['/static/assets/alphabets/X-img/xylophone.jpg', 'xylophone'],
 
-['./assets/alphabets/Y-img/yatch.jpg', 'yatch'],
-['./assets/alphabets/Y-img/yak.webp', 'yak'],
+['/static/assets/alphabets/Y-img/yatch.jpg', 'yatch'],
+['/static/assets/alphabets/Y-img/yak.webp', 'yak'],
 
-['./assets/alphabets/Z-img/zebra.png', 'zebra'],
-['./assets/alphabets/Z-img/zip.jpg', 'zip'],
+['/static/assets/alphabets/Z-img/zebra.png', 'zebra'],
+['/static/assets/alphabets/Z-img/zip.jpg', 'zip'],
 ]
